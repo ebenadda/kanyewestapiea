4dd1bbd200e480cced049af31cae2f31fbc1e3f9
ebenadda <ebenadda@amazon.com>
2022-07-21 09:49:35 -0400

    added comments to clarify code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import axios from "axios";
-import express from "express";
-import { pathToFileURL } from "url";
-const app = express();
+import axios from "axios"; //Imports axios which is used to obtain the kanye_rest URL
+import express from "express"; //Imports the express framework which provides the features used in node
 import Cors from "cors";
 
-const PORT = process.env.PORT || 8000;
+const app = express(); // This is a declaration that app refers to the express function.
+
+const PORT = process.env.PORT || 8000; // This is a declaration of PORT, stating that port could listen on channel 8000 or on any channel provided by the platform.
+
+//This function tells the app to list on the PORT stated.
 app.listen(PORT, () => {
   console.log(`Server listening on port:${PORT}`);
 });
 
-app.use(Cors());
+app.use(Cors()); // This simply says the app is using Cors
 
 // app.use((req, res, next) => {
 //   console.log("Some end poing hit");
 //   next();
 // });
 
-const URL = "https://api.kanye.rest/";
+const URL = "https://api.kanye.rest/"; // This is a declaration of the kanye_rest url
 
-//TODO go to the url and fetch the data thast there
-
-//GET /Kanye_quotes
+//GET /Kanye_quotes tells the app to go to the url and fetch the data thast there and send it back in the console or log error if there is an error.
 //<SERVERNAME>.<METHODNAME>.(<PATHNAME>, <CALLBACK FUNTION>)
 app.get(
   "/kanye_quotes",
@@ -38,6 +38,7 @@ app.get(
   }
 );
 
+// This is telling app to get /eben and send a response
 app.get("/eben", (req, res) => {
   res.send("Hello Eben");
 });
